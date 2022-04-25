053a1258d595aa42fbae48a9a4e2899ccfbb9c35
SamakMashwee <samakmashwee@gmail.com>
2022-04-25 07:27:22 -0500

    updates

--- graph.py
+++ graph.py
@@ -1,4 +1,24 @@
 import json
+from math import sin
+
+def function(x):
+    return sin(x)
+
+def derive(x):
+    return 
+
+def close_to(x, y, deviation):
+    return abs(x-y)<deviation
+
+def snap_val(y, pos, range):
+    steps = range/8
+    goal = y-(pos-(range/2))
+    counter = 0
+    step = 0
+    while step < goal:
+        step += steps
+        counter+=1
+    return counter-1
 
 class pixel():
     def __init__(self, x, y):
@@ -7,7 +27,7 @@ class pixel():
     display = ''
     def shade(self):
         self.display = '#'
-
+    
 class ascii_display():
     def __init__(self, params):
         self.height = params["height"]
@@ -30,14 +50,18 @@ class ascii_display():
     def draw_graph(self):
         for i in range(self.height):
             for j in range(self.width):
-                if self.screen[i][j].x == 0 and self.screen[i][j].y == 0:
+                if close_to(self.screen[i][j].x, 0, self.xsteps/2) and close_to(self.screen[i][j].y, 0, self.ysteps/2):
                     self.screen[i][j].display = '+'
-                elif self.screen[i][j].x == 0:
+                elif close_to(self.screen[i][j].x, 0, self.xsteps/2):
                     self.screen[i][j].display = '|'
-                elif self.screen[i][j].y == 0:
+                elif close_to(self.screen[i][j].y, 0, self.ysteps/2):
                     self.screen[i][j].display = '-'
                 else:
                     self.screen[i][j].display = ' '
+                
+                if close_to(self.screen[i][j].y, function(self.screen[i][j].x), self.ysteps/2):
+                    table = "_,.-~*'`"
+                    self.screen[i][j].display = table[int(snap_val(function(self.screen[i][j].x), self.screen[i][j].y, self.ysteps))]
 
     def show_display(self):
         for i in range(self.height):
--- notation.py
+++ notation.py
@@ -1,27 +1,54 @@
 import operator
 import re
+import enum
+
+class token_type(enum.Enum):
+    T_NUM = 0
+    T_ADD = 1
+    T_SUB = 2
+    T_MUL = 3
+    T_DIV = 4
+    T_POW = 5
+    T_LPAR = 6
+    T_RPAR = 7
+    T_END = 8
 
 operations = { '*' : operator.mul, '/' : operator.truediv, '+' : operator.add, '-' : operator.sub, '^' : pow }
+token_map = { '*' : token_type.T_MUL, '/' : token_type.T_DIV, '+' : token_type.T_ADD, '-' : token_type.T_SUB, '^' : token_type.T_POW }
+
+class node:
+    def __init__(self, type, data=None):
+        self.type = type
+        self.data = data
+        self.children = []
+
 class data:
     def __init__(self, input):
         self.input = input
     
     def tokenize(self):
-        self.tokens = []
+        self.strtkns = []
         for counter in range(len(self.input)):
             if self.input[counter].isdigit():
                 tmpstr = ""
                 while self.input[counter].isdigit():
                     tmpstr += self.input[counter]
                     counter+=1
-                self.tokens.append(tmpstr)
+                self.strtkns.append(tmpstr)
             
             else:
-                self.tokens.append(self.input[counter])
+                self.strtkns.append(self.input[counter])
 
-    def mkstring(self):
-        self.input = self.input.join(self.input.split())
-
-        self.tokenize()
-        for token in self.tokens:
-            
+    def construct_ast(self):
+        self.tokens = []
+        for c in self.input:
+            if c in token_map:
+                token = node(token_map[c], data=c)
+            elif re.match(r'\d', c):
+                token = node(token_type.T_NUM, data=int(c))
+            else:
+                raise Exception(f"Error Invalid Token: {token}")
+            self.tokens.append(token)
+        self.tokens.append(node(token_type.T_END))
+        
+        
